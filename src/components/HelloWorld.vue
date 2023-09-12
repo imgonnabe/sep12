b78867190c2825c59fc54e9fdf67fd612870a2ba
imgonnabe <qorlwn456123@gmail.com>
2023-09-12 12:51:54 +0900

    vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,34 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1>{{msg}}</h1>
+    <span v-text="msg"></span>
   </div>
+  <div>{{human.name}}</div>
+  <div>{{human.age}}</div>
+  <div>{{human.addr}}</div>
+  <div>{{arr}}</div>
+  <div>{{arr[0]}}</div>
+  <div v-if="arr[1] == 10">10입니다.</div>
+  <div v-else-if="arr[0] == 12">12입니다.</div>
+  <div v-else>10이 아닙니다.</div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
+  /*props: {// 부모의 값을 자식이 찍을 때
     msg: String
+  }*/
+  data(){
+    return{
+      msg:"자식에서 값을 보냅니다.",
+      human : {
+        name: "홍길동",
+        age: 150,
+        addr: "한양"
+      },
+      arr:[10,20,30,40,50,60,70,80,90,100],
+    }
   }
 }
 </script>
